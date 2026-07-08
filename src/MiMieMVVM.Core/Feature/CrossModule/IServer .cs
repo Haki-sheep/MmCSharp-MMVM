d9f19a204ace4bdd30a38dce45024c56493f9e8d
Haki-sheep <1935207581@qq.com>
2026-07-08 22:51:08 +0800

    定好契约

--- src/MiMieMVVM.Core/Feature/CrossModule/IServer .cs
+++ src/MiMieMVVM.Core/Feature/CrossModule/IServer .cs
@@ -9,8 +9,27 @@ namespace MiMieMVVM
     /// 跨模块服务应该继承此接口
     /// 在里面写跨模块服务的方法
     /// </summary>
-    public interface IServer 
+    public interface ICrossBusinessModuleService
     {
-        
+    }
+
+    public class BusinessModuleHub
+    {
+        private static BusinessModuleHub instance = new BusinessModuleHub();
+        public static BusinessModuleHub Instance => instance ??= new BusinessModuleHub();
+        private readonly Dictionary<Type, ICrossBusinessModuleService> bsDict = new();
+        public void RegisterBusinessModule<T>(T service) where T : class, ICrossBusinessModuleService
+        {
+            bsDict[typeof(T)] = service;
+        }
+        public T GetBusinessModule<T>() where T : class, ICrossBusinessModuleService
+        {
+            return (T)bsDict[typeof(T)];
+        }
+
+        public void UnregisterBusinessModule<T>() where T : class, ICrossBusinessModuleService
+        {
+            bsDict.Remove(typeof(T));
+        }
     }
 }
